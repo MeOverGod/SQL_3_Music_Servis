ebcb21134daac3a4e22d689f92db21d7f8922638
Pavel Selekhov <mrovergod@gmail.com>
2023-10-07 11:54:13 +0300

    cheange .sql

--- Текстовый документ.sql
+++ Текстовый документ.sql
@@ -52,70 +52,44 @@ CREATE TABLE IF NOT EXISTS Song_Collection (
 --Исполнитель (имя)
 
 INSERT INTO Artist(name)
-VALUES('Latimore');
-
-INSERT INTO Artist(name)
-VALUES('Hi-Fi');
-
-INSERT INTO Artist(name)
-VALUES('alt-J');
-
-INSERT INTO Artist(name)
-VALUES('Van Halen');
-
-INSERT INTO Artist(name)
-VALUES('IC3PEAK'), ('Хаски');
+VALUES
+	('Latimore'),
+	('Hi-Fi'),
+	('alt-J'),
+	('Van Halen'),
+	('IC3PEAK'),
+	('Хаски');
 
 --Альбом (имя, год)
 
 INSERT INTO Albom(name, year)
-VALUES('It Ain''t Where You Been', 1974);
-
-INSERT INTO Albom(name, year)
-VALUES('The Best (Легенды Русского Радио)', 2004);
-
-INSERT INTO Albom(name, year)
-VALUES('An Awesome Wave', 2012);
-
-INSERT INTO Albom(name, year)
-VALUES('Best of Volume 1', 1996);
-
-INSERT INTO Albom(name, year)
-VALUES('До свидания', 2020);
+VALUES
+	('It Ain''t Where You Been', 1974),
+	('The Best (Легенды Русского Радио)', 2004),
+	('An Awesome Wave', 2012),
+	('Best of Volume 1', 1996),
+	('До свидания', 2020);
 
 --Исполнитель_Альбом (id исполнитель, id альбома)
 
 INSERT INTO Artist_Albom(artist_id, albom_id)
-VALUES(1, 1);
-
-INSERT INTO Artist_Albom(artist_id, albom_id)
-VALUES(2,2);
-
-INSERT INTO Artist_Albom(artist_id, albom_id)
-VALUES(3,3);
-
-INSERT INTO Artist_Albom(artist_id, albom_id)
-VALUES(4, 4);
-
-INSERT INTO Artist_Albom(artist_id, albom_id)
-VALUES(5, 5), (6, 5);
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(6, 5);
 
 --Песня (имя, продолжительность, id альбома)
 
 INSERT INTO Song(name, duration, albom_id)
-VALUES('Let Me Go', 184, 1);
-
-INSERT INTO Song(name, duration, albom_id)
-VALUES('Седьмой лепесток', 191, 2);
-
-INSERT INTO Song(name, duration, albom_id)
-VALUES('Breezeblocks', 227, 3);
-
-INSERT INTO Song(name, duration, albom_id)
-VALUES('Fitzpleasure', 219, 3);
-
-INSERT INTO Song(name, duration, albom_id)
-VALUES('Jump', 239, 4);
+VALUES
+	('Let Me Go', 184, 1),
+	('Седьмой лепесток', 191, 2),
+	('Breezeblocks', 227, 3),
+	('Fitzpleasure', 219, 3),
+	('Jump', 239, 4);
 
 INSERT INTO Song(name, duration, albom_id)
 VALUES('Eruption', 102, 4);
@@ -126,86 +100,51 @@ VALUES('Весело и грустно', 194, 5);
 --Сборник (имя, год)
 
 INSERT INTO Collection(name, year)
-VALUES('Собрание классики', 2016);
-
-INSERT INTO Collection(name, year)
-VALUES('Сборник 1', 2017);
-
-INSERT INTO Collection(name, year)
-VALUES('Сборка', 2012);
-
-INSERT INTO Collection(name, year)
-VALUES('Песенный набор', 2023);
-
-INSERT INTO Collection(name, year)
-VALUES('Песня года', 2019);
+VALUES
+	('Собрание классики', 2016),
+	('Сборник 1', 2017),
+	('Сборка', 2012),
+	('Песенный набор', 2023),
+	('Песня года', 2019);
 
 --Песня_Сборник (id песни, id сборника)
 
 INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(5, 1);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(7, 1);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(2, 1);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(3,2);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(4,3);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(6, 4);
-
-INSERT INTO Song_Collection(song_id, collection_id)
-VALUES(3, 5);
+VALUES
+	(5, 1), 
+	(7, 1),
+	(2, 1),
+	(3, 2),
+	(4, 3),
+	(6, 4),
+	(3, 5);
 
 --Жанр (имя)
 
 INSERT INTO Genre(name)
-VALUES('Blues');
-
-INSERT INTO Genre(name)
-VALUES('Pop');
-
-INSERT INTO Genre(name)
-VALUES('Indie');
-
-INSERT INTO Genre(name)
-VALUES('Rock');
-
-INSERT INTO Genre(name)
-VALUES('Rap');
+VALUES
+	('Blues'),
+	('Pop'),
+	('Indie'),
+	('Rock'),
+	('Rap');
 
 --Исполнитель_Жанр (id исполнитель, id жанра)
 
 INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(1, 1);
-
-INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(2,2);
-
-INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(3,3);
-
-INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(4, 4);
-
-INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(5, 3);
-
-INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES(6, 5);
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 3),
+	(6, 5);
 
 --SELECT 1
 
 --Название и продолжительность самого длительного трека
 SELECT name, duration FROM Song
-ORDER BY duration DESC
-LIMIT 1;
+WHERE duration = (SELECT MAX(duration) FROM Song);
 
 --Название треков, продолжительность которых не менее 3,5 минут
 SELECT name FROM Song
@@ -240,17 +179,16 @@ FULL JOIN albom a ON a.id = s.albom_id
 GROUP BY a.name;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году
-SELECT t.name FROM artist t
+SELECT t.name, a.year FROM artist t
 FULL JOIN artist_albom aa ON t.id = aa.artist_id
 FULL JOIN albom a ON a.id = aa.albom_id
-WHERE year != 2020;
+WHERE t.name NOT IN (SELECT t.name FROM artist_albom WHERE a.year = 2020);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
-SELECT c.name FROM collection c
-FULL JOIN song_collection sc ON c.id = sc.collection_id
-FULL JOIN song s ON s.id = sc.song_id
-FULL JOIN albom a ON a.id = s.albom_id
-FULL JOIN artist_albom aa ON a.id = aa.albom_id
-FULL JOIN artist t ON t.id = aa.artist_id
-WHERE t.name = 'Hi-Fi'
-GROUP BY c.name;
+SELECT DISTINCT c.name FROM collection c
+JOIN song_collection sc ON c.id = sc.collection_id
+JOIN song s ON s.id = sc.song_id
+JOIN albom a ON a.id = s.albom_id
+JOIN artist_albom aa ON a.id = aa.albom_id
+JOIN artist t ON t.id = aa.artist_id
+WHERE t.name = 'Hi-Fi';
